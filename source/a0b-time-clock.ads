7da7d82bcf672134da9b30767985958eb57bae15
Vadim Godunko <vgodunko@gmail.com>
2024-04-21 16:19:44 +0400

    Specification file for Clock function.

---
+++ source/a0b-time-clock.ads
@@ -0,0 +1,11 @@
+--
+--  Copyright (C) 2024, Vadim Godunko <vgodunko@gmail.com>
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+--
+
+--  Function to get current monotonic time.
+
+pragma Restrictions (No_Elaboration_Code);
+
+function A0B.Time.Clock return A0B.Time.Monotonic_Time with Inline_Always;
